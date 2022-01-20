8d6773a08f5c559aa6da083876144533620f8a21
VitorDiass <vitordias28@hotmail.com>
2022-01-20 22:37:42 +0000

    server branch

--- mobileapp/graphql/apollo/apollo.ts
+++ mobileapp/graphql/apollo/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
 /* import { onError } from 'apollo-link-error'; */
 
 //const URI = "http://192.168.221.238:4000/";
-const URI = "http://192.168.244.253:4444/";
+const URI = "http://192.168.244.253:4000/";
 
 /* const errorLink = onError(({ graphQLErrors }) => {
     if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message))
