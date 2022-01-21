e9acdfdafb5ad34456c2fd50f4362f2f54237929
VitorDiass <vitordias28@hotmail.com>
2022-01-21 19:18:10 +0000

    package.json

--- server/build/server.js
+++ server/build/server.js
@@ -9,23 +9,24 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 exports.__esModule = true;
-var graphql_file_loader_1 = require("@graphql-tools/graphql-file-loader");
-var load_1 = require("@graphql-tools/load");
+//import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+//import { loadSchemaSync } from '@graphql-tools/load';
 var schema_1 = require("@graphql-tools/schema");
 var apollo_server_1 = require("apollo-server");
 var graphql_scalars_1 = require("graphql-scalars");
 var resolvers_1 = require("./resolvers");
-var typeDefs = (0, load_1.loadSchemaSync)('./**/*.graphql', {
-    loaders: [
-        new graphql_file_loader_1.GraphQLFileLoader()
+//const typeDefs = loadSchemaSync('./**/*.graphql',{
+/*    loaders : [
+        new GraphQLFileLoader()
     ]
-});
+}) */
 var schema = (0, schema_1.makeExecutableSchema)({
-    typeDefs: __spreadArray(__spreadArray([], graphql_scalars_1.typeDefs, true), [typeDefs], false),
+    typeDefs: __spreadArray([], graphql_scalars_1.typeDefs, true),
     resolvers: __spreadArray([graphql_scalars_1.resolvers], resolvers_1.Resolvers, true)
     //Date : GraphQLDate,
 });
-var server = new apollo_server_1.ApolloServer({ cors: { origin: 'https://studio.apollographql.com', methods: 'POST' }, schema: schema });
+var server = new apollo_server_1.ApolloServer({ cors: { origin: '*', methods: 'POST' }, schema: schema });
+//
 server.listen({ port: process.env.PORT || 4444 }).then(function (_a) {
     var url = _a.url;
     return console.log("server is ready at " + url);
